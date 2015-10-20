c212a682540118a3538931015aa00999dc928272
Anton <avtonomspb@gmail.com>
2015-10-20 15:22:25 +0300

    add package

--- AvtonomMediaStorageClientBundle.php
+++ AvtonomMediaStorageClientBundle.php
@@ -4,6 +4,10 @@ namespace Avtonom\MediaStorageClientBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class AvtonomMediaStorageClientBundle
+ * @package Avtonom\MediaStorageClientBundle
+ */
 class AvtonomMediaStorageClientBundle extends Bundle
 {
 }
