c08a4b1a6bd348cc9797f1506f3684d8414f1ef4
Anton <avtonomspb@gmail.com>
2015-10-27 14:29:25 +0300

    rename MediaStorageClientChangeFieldListener

--- EventListener/MediaStorageClientChangeFieldListener.php
+++ EventListener/MediaStorageClientChangeFieldListener.php
@@ -17,6 +17,8 @@ use Symfony\Bridge\Monolog\Logger;
  * @todo validate url - backend
  * @todo recucle http://www.whitewashing.de/2013/07/24/doctrine_and_domainevents.html
  * @todo two file listener and run send
+ * @todo move config interfaces to use Trait
+ * @todo use wrapper to set/get field
  */
 class MediaStorageClientChangeFieldListener
 {
